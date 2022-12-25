75d655bdbcd634e7a1c25d2e256b45a8ada0afdd
ilkintagiyev <ilkin.tito.15@list.ru>
2022-12-25 20:47:47 +0400

    asfasfsafsa

---
+++ src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useState } from 'react'
+import { questions } from '../../utils/data';
+import style from './Quiz.module.css';
+
+const Quiz = () => {
+
+    const [index, setIndex] = useState(Math.floor(Math.random() * questions?.length))
+    const [quizes, setQuizes] = useState(questions);
+    const [answer, setAnswer] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [wrong, setWrong] = useState(false);
+
+    const showAnswer = useCallback(() => {
+        setAnswer(quizes[index].answer)
+    }, [index, quizes])
+
+    const submitAnswer = useCallback(() => {
+        if (inputValue === quizes[index].answer) {
+            setWrong(false)
+            setIndex(Math.floor(Math.random() * questions?.length))
+        }
+        else {
+            setWrong(true)
+        }
+        setInputValue('')
+    }, [index, inputValue, quizes])
+
+    return (
+        <div className={style.container}>
+            <h2 className={style.question}>{quizes[index].question}</h2>
+            <label className={style.label}>
+                <input onChange={e => setInputValue(e.target.value)} placeholder='Cavabı yaz' />
+                <button onClick={submitAnswer}>Təsdiqlə</button>
+                <button onClick={showAnswer}>Cavabı göstər</button>
+            </label>
+            <h3>{answer}</h3>
+            <h3>{wrong && 'Səhv yazdınız zəhmət olmasa yenidən yoxlayın'}</h3>
+        </div>
+    )
+}
+
+export default Quiz
---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        id : 1,
+        question : 'Privileged nədir',
+        answer : 'imtiyazlı',
+    },
+    {
+        id : 2,
+        question : 'Clapping nədir',
+        answer : 'əl çalmaq'
+    },
+    {
+        id : 3,
+        question : 'sincere',
+        answer : 'mənzərə'
+    },
+    {
+        id : 4,
+        question : 'integrity',
+        answer : 'bütövlük'
+    }
+]
